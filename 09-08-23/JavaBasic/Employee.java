f4b1c44f2eb57114f3fca4655a0a5fc4d30f6c64
Amjad <75468288+mjsgitt@users.noreply.github.com>
2024-02-09 12:16:08 +0530

    Added The First Completion of the Section
    Java Basics

---
+++ 09-08-23/JavaBasic/Employee.java
@@ -0,0 +1,18 @@
+public class Employee{
+    String name;
+    int age;
+    double salary;
+    String location;
+
+    Employee(String Name,int Age,double Salary,String Location){
+        this.name = Name;
+        this.age = Age;
+        this.salary = Salary;
+        this.location = Location;
+    }
+
+    public void GetSalary(){
+        System.out.println(salary);
+    }
+    
+}
---
+++ 09-08-23/JavaBasic/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[]args){
+        Employee Emp1 = new Employee("Naresh", 50 , 50000 , "Banglore");
+        Emp1.GetSalary();
+    }
+}
